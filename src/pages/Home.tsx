4504ea103c2bbf4315120892347f1208d4ebebb2
Michael Munsie <mikemunstrocity@gmail.com>
2021-09-01 21:57:55 -0500

    Add retro vette wallpapere

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,7 +3,9 @@ export default function Home() {
     <div>
       <a className="button mb-2" href="https://www.instagram.com/aluc4rd.vette/">Instagram</a>
       <a className="button mb-2" href="https://www.thegearheadproject.com/2021/corvette-americas-sports-car/">Interview with The Gearhead Magazine</a>
-      <a className="button" href="#/parts">Parts + Mods List</a>
+      <a className="button mb-2" href="#/parts">Parts + Mods List</a>
+      <a className="button mb-1" rel="noreferrer" target="_blank" href={process.env.PUBLIC_URL + "/retrovette.jpg"}>Download Retro Vette Wallpaper</a>
+        <em>^ Also great for custom cell phone skins!</em>
     </div>
   )
 }
